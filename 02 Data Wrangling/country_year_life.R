c6067ad2dc1cf94828a2a2ff4a59e1321b992665
Kevin Nievera <kevin.nievera@gmail.com>
2015-02-16 17:00:49 -0600

    edited country_year_life.R

--- 02 Data Wrangling/country_year_life.R
+++ 02 Data Wrangling/country_year_life.R
@@ -1,3 +1,8 @@
 #country, year, life expectancy of both sexes
 
-life <- lifeData %>% filter(INDICATOR=='Life expectancy at birth (years)', SEX=='Both sexes') %>% group_by(COUNTRY, YEAR) %>% select(LIFE_EXPECTANCY) %>% arrange(COUNTRY, YEAR)  %>%  tbl_df
+
+life <- lifeData %>% filter(INDICATOR=='Life expectancy at birth (years)') %>% group_by(COUNTRY, YEAR) %>% select(LIFE_EXPECTANCY) %>% arrange(COUNTRY, YEAR)  %>% tbl_df() 
+
+ggplot(life, aes(x=COUNTRY, y=LIFE_EXPECTANCY)) + geom_point(aes(color=YEAR))
+
+
