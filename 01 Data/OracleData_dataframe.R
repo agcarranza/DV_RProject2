e01a23fa8e46412cc5437365beae4ef9b9a93b75
Jorge Paramo <jrgparamo@utexas.edu>
2015-02-16 23:17:57 -0600

    Created my workflow, fixed .rprofile, and added my ggplot

--- 01 Data/OracleData_dataframe.R
+++ 01 Data/OracleData_dataframe.R
@@ -1,6 +1,14 @@
+require(ggplot2)
+require(gplots)
+require(plyr)
+require(grid)
 require(RCurl)
+require(reshape2)
+require(rstudio)
+require(tableplot)
+require(tidyr)
 require(dplyr)
 
 lifeData <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from LIFE_EXPECTANCY"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_jp33534', PASS='orcl_jp33534', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
 
-tbl_df(lifeData)
+knitr::kable(head(lifeData), caption = "Life Expectancy")
